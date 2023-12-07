3a449535855f1742948f1bba94d6bea82f8352dd
Ajay-Kumbhar <93241196+Ajay-Kumbhar@users.noreply.github.com>
2023-12-07 17:46:09 +0530

    Create database.php

---
+++ dbconnection/database.php
@@ -0,0 +1,12 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'ajay');
+    define('DB_PASS', 'root');
+    define('DB_NAME', 'bloodbankdb');
+
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+    if($conn->connect_error){
+        die('Connection failed').$conn->connect_error;
+    }
+?>
